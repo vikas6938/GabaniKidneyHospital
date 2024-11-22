4eae755a041c43bcaea04513ec30afa4faacf485
vikas6938 <vikasborse000@gmail.com>
2024-11-23 05:13:09 +0530

    Main banner complete

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    content: [
+      './*.html', // Include all HTML files in the root directory
+      './components/**/*.html', // Include all HTML files in the components folder
+      './assets/js/**/*.js', // Include JavaScript files for dynamic class usage
+    ],
+    theme: {
+      extend: {
+        colors: {
+          primary: "#009DDD",
+          secondary: "#747474",
+        },
+        spacing: {
+          240: "240px",
+          40: "40px",
+        },
+        maxWidth: {
+          container: "1440px",
+        },
+        fontFamily: {
+          barlow: ["'Barlow Condensed'", "sans-serif"],
+        },
+      },
+    },
+    plugins: [],
+  };
+  
