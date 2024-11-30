8399410b0e8189efcf6ea8ec4ce0e3c67c8509c8
vikas6938 <vikasborse000@gmail.com>
2024-12-01 05:11:52 +0530

    responsive galary page

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,19 +18,4 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-// Get the hamburger button, off-canvas menu, and close button
-const hamburgerBtn = document.getElementById('hamburger-btn');
-const offCanvasMenu = document.getElementById('off-canvas-menu');
-const closeMenuBtn = document.getElementById('close-menu');
 
-// Open the off-canvas menu when hamburger button is clicked
-hamburgerBtn.addEventListener('click', () => {
-  offCanvasMenu.classList.remove('translate-x-full'); // remove class to slide in
-  offCanvasMenu.classList.add('translate-x-0'); // add class to bring it in
-});
-
-// Close the off-canvas menu when close button is clicked
-closeMenuBtn.addEventListener('click', () => {
-  offCanvasMenu.classList.remove('translate-x-0'); // remove class to slide out
-  offCanvasMenu.classList.add('translate-x-full'); // add class to bring it off-screen
-});
